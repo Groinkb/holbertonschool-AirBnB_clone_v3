f272128225fd711092871c90be92c2a00be78989
Groinkb <benjamin@mossu.ch>
2023-12-22 21:30:01 +0100

    crampté

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
-"""Main module to start the Flask application"""
+"""
+Runs the app.py file
+"""
 
-from flask import Flask, jsonify
+
+from flask import Flask
+from flask import jsonify
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Teardown method to close the storage"""
+def teardown_appcontext(exception):
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """404 error with json print"""
+def handle_error(e):
     return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(getenv("HBNB_API_PORT", 5000))
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
